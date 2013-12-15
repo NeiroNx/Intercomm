80fa55f1d883e1477a70c381bc962dcf78dc8117
U-NeiroN-NT\NeiroN <NeiroN@NeiroN-NT.(none)>
2013-12-15 23:28:35 +0400

    Initial commit

---
+++ Intercomm/src/main/java/com/nxn/intercomm/MainActivity.java
@@ -0,0 +1,299 @@
+package com.nxn.intercomm;
+
+import java.text.DecimalFormat;
+import java.text.Format;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.text.Editable;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+public class MainActivity extends ActionBarActivity implements ActionBar.TabListener,OnClickListener {
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link FragmentPagerAdapter} derivative, which will keep every
+     * loaded fragment in memory. If this becomes too memory intensive, it
+     * may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    ViewPager mViewPager;
+    ToggleButton mEnable;
+    Fragment mManual;
+    Fragment mChannels;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Set up the action bar.
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setIcon(R.drawable.ic_launcher);
+        actionBar.setCustomView(R.layout.actionbar);
+        mManual = new ManualFrequency();
+        mChannels = new Channels();
+        mEnable = (ToggleButton)actionBar.getCustomView().findViewById(R.id.enable);
+        mEnable.setOnClickListener(this);
+
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        // When swiping between different sections, select the corresponding
+        // tab. We can also use ActionBar.Tab#select() to do this if we have
+        // a reference to the Tab.
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                actionBar.setSelectedNavigationItem(position);
+            }
+        });
+
+        // For each of the sections in the app, add a tab to the action bar.
+        for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
+            // Create a tab with text corresponding to the page title defined by
+            // the adapter. Also specify this Activity object, which implements
+            // the TabListener interface, as the callback (listener) for when
+            // this tab is selected.
+            actionBar.addTab(
+                    actionBar.newTab()
+                            .setText(mSectionsPagerAdapter.getPageTitle(i))
+                            .setTabListener(this));
+        }
+    }
+
+    @Override
+    public void onClick(View view){
+        if(view.getId() == R.id.enable){
+            EditText ed = (EditText)mManual.getView().findViewById(R.id.freq);
+            if(mEnable.isChecked()){
+                ed.setText("333");
+            }else{
+                ed.setText("222");
+            }
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()){
+            case R.id.action_settings:
+                return true;
+            case R.id.action_quit:
+
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+        // When the given tab is selected, switch to the corresponding page in
+        // the ViewPager.
+        mViewPager.setCurrentItem(tab.getPosition());
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            switch (position){
+                case 0:
+                    return mManual;
+                case 1:
+                    return mChannels;
+            }
+            return PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            Locale l = Locale.getDefault();
+            switch (position) {
+                case 0:
+                    return getString(R.string.title_section1).toUpperCase(l);
+                case 1:
+                    return getString(R.string.title_section2).toUpperCase(l);
+                case 2:
+                    return getString(R.string.title_section3).toUpperCase(l);
+            }
+            return null;
+        }
+    }
+    public class ManualFrequency extends Fragment
+            implements OnClickListener,TextWatcher {
+        ImageButton next;
+        ImageButton prew;
+        EditText freq;
+        NumberFormat Format;
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView;
+            rootView = inflater.inflate(R.layout.manual_freq, container, false);
+            next = (ImageButton) rootView.findViewById(R.id.freq_next);
+            next.setOnClickListener(this);
+            prew = (ImageButton) rootView.findViewById(R.id.freq_perw);
+            prew.setOnClickListener(this);
+            freq = (EditText) rootView.findViewById(R.id.freq);
+            Format = NumberFormat.getInstance(Locale.ENGLISH);
+            ((DecimalFormat)Format).applyPattern("###.#####");
+            Format.setMinimumFractionDigits(5);
+            Format.setMinimumIntegerDigits(3);
+            return rootView;
+        }
+        @Override
+        public void onTextChanged(CharSequence str, int a,int b,int c){
+            freq.setText("1"+str);
+        }
+        @Override
+        public void beforeTextChanged(CharSequence str, int a,int b,int c){
+            freq.setText("2"+str);
+        }
+        @Override
+        public void afterTextChanged(Editable ed){
+            freq.setText(verify(ed.toString()));
+        }
+        @Override
+        public void onClick(View view){
+            Double num;
+            try {
+                num = Double.parseDouble(freq.getText().toString());
+            }catch (Exception error){
+                num = 0.0;
+            }
+            switch (view.getId()){
+                case R.id.freq_perw:
+                    freq.setText(verify(Format.format(num - 0.00025)));
+                    freq.selectAll();
+                    break;
+                case R.id.freq_next:
+                    freq.setText(verify(Format.format(num + 0.00025)));
+                    freq.selectAll();
+                    break;
+            }
+        }
+        private String verify(String str){
+            Double num = Double.parseDouble(str);
+            if(num < 400.0) num = 400.00000;
+            if(num > 480.0) num = 480.00000;
+            return Format.format(num);
+        }
+    }
+    public class Channels extends Fragment {
+        public Channels() {
+        }
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView;
+            rootView = inflater.inflate(R.layout.channels, container, false);
+            return rootView;
+        }
+    }
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView;
+                    rootView = inflater.inflate(R.layout.fragment_main, container, false);
+                    TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+                    textView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+    }
+
+}
